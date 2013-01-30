18c191c2fff3e7490e9f73127eeba86ca5592f3e
Leo.Chen <leochen1931@gmail.com>
2013-01-30 22:38:39 +0800

    双色球选号1.0版本，功能只有一个，通过一个简单算法

---
+++ LotterySSQ/LeoData.m
@@ -0,0 +1,13 @@
+//
+//  LeoData.m
+//  LotterySSQ
+//
+//  Created by Leo.Chen on 13-1-26.
+//  Copyright (c) 2013年 Leo.Chen. All rights reserved.
+//
+
+#import "LeoData.h"
+
+@implementation LeoData
+
+@end
